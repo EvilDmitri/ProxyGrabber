a7357dd0a978cf5f5b5e53914e1eb0526eb579b4
EvilDmitri <dmitribrach@gmail.com>
2012-04-03 23:21:16 +0300

    modified:   grabber.py added YAPH

--- grabber.py
+++ grabber.py
@@ -1,11 +1,17 @@
 # /usr/bin/env/python
 # -*-coding=utf-8-*-
-
+import subprocess
 import re
 # Проcтое решение «в лоб», проверяющее не только цифры но и выход за пределы допустимых диапазонов
 #?   ip4_re = re.compile(r'^(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}$')
 #???
 
+
+#pattern = "((?P<login>\w+):(?P<password>\w+)@)?(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})(:(?P<port>\d+))?"
+
+#re.match(pattern, "12.34.56.789").groupdict()
+#re.match(pattern, "12.34.56.789:80").groupdict()
+#re.match(pattern, "john:pass@12.34.56.789:80").groupdict()
 
 from grab import Grab
 
@@ -17,7 +23,11 @@ file_site_list = open('site_list.txt', 'r')
 
 line_number = 1  # ))) Только для вывода в консоль
 
-for site_address in file_site_list:
+for site in file_site_list:
+    # Разобрать строку на socks_type и site_address
+    temp = site.split()
+    socks_type = temp[0] + "\t"
+    site_address = temp[1]
     
     g.go(site_address)
 
@@ -32,26 +42,45 @@ for site_address in file_site_list:
     
     
 # ProxyList format
-#       type  host  port [user pass]
-#       (values separated by 'tab' or 'blank')
-    file_to_write = open("ip-port.txt", "w")
+#    type  host  port [user pass]
+
+    file_to_write = open("ip-port.txt", "a")
+
     for elem in ip_port_list:
-        elem.replace(":", " ") # !!!!!!
-        file_to_write.write("socks5 " + elem + "\n")
+        elem = elem.replace(":", "\t")
+        file_to_write.write(socks_type + elem + "\n")
+        
     file_to_write.close()
     
     print "{} line done".format(line_number)
     line_number += 1
 
+file_site_list.close()
+print "All sites done."
+print "Now checking addresses for..."
+
+
+# Проверка адресов на рабочесть)))
+# Используется YAPH через proxychains
+
+proc = subprocess.Popen("cat ip-port.txt | sort | uniq | proxychains yaph --use_hunter_stdin", shell=True, stdout=subprocess.PIPE)
+
+#for output in proc.stdout.readline(): #читаем строку - вывод 
+is_data = True
+while is_data: 
+    data = proc.stdout.readline()
+    if data:
+        print data
+    else: is_data = False
+
+print "It's over"
+
+# удалить файл ip-port.txt
+os.remove("ip-port.txt")
 
 
 
 
-#pattern = "((?P<login>\w+):(?P<password>\w+)@)?(?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})(:(?P<port>\d+))?"
-
-#re.match(pattern, "12.34.56.789").groupdict()
-#re.match(pattern, "12.34.56.789:80").groupdict()
-#re.match(pattern, "john:pass@12.34.56.789:80").groupdict()
 
 
 
